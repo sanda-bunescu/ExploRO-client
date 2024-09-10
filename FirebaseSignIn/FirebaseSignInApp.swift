caa8faf5bbe1c8fcd77278b2694d7188897a7098
sanda-bunescu <sanda.accounts@proton.me>
2024-09-10 11:20:31 +0300

    Initial Commit

---
+++ FirebaseSignIn/FirebaseSignInApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSignInApp.swift
+//  FirebaseSignIn
+//
+//  Created by Sanda Bunescu on 10.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseSignInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
